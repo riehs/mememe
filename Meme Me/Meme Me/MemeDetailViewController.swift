a5f233e9eed09ca44625114c78935793b60d75d1
riehs <riehs@bc.edu>
2015-03-16 00:04:02 -0400

    Committed by Udacity

---
+++ Meme Me/Meme Me/MemeDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MemeDetailViewController.swift
+//  BondVillains
+//
+//  Created by Daniel Riehs on 3/14/15.
+//  Copyright (c) 2015 Daniel Riehs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeDetailViewController: UIViewController
+{
+    var meme: Meme!
+    
+    @IBOutlet weak var memedImage: UIImageView!
+    
+    override func viewWillAppear(animated: Bool) {
+        memedImage.image = meme.memedImage
+    }
+    
+}
